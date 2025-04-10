4cec2c985128660d6e5a3124d26dcb247fcf6293
rubens junio <rubens.marques@hotmart.com>
2025-04-10 12:52:34 -0300

    run lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
 import { AppRoutes } from './app/Routes'
 
 import './assets/styles/global.css'
