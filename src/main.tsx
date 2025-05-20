c7b80193c15c4a68bb068d228ff68b44b19c93c7
rubens junio <rubens.marques@hotmart.com>
2025-05-20 20:43:34 -0300

    refactor: atualização dos caminhos de importação para a nova estrutura de pastas

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-import { RoutesComponent } from './routes/Routes'
-import { AuthProvider } from './context/AuthContext'
+import { RoutesComponent } from '@/routes/Routes'
+import { AuthProvider } from '@/context/AuthContext'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/queryClient'
+import { queryClient } from '@/lib/queryClient'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import './config/i18n'
-import './assets/styles/global.css'
+import '@/config/i18n'
+import '@/assets/styles/global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
