2ec53fa8ff425f3c8215ec5de3876ef2ab11ce80
rubens junio <rubens.marques@hotmart.com>
2025-10-01 10:04:23 -0300

    trash dropdown menu

--- src/shared/components/Atoms/DropdownMenu/DropdownMenuGroup.tsx
+++
@@ -1,8 +0,0 @@
-import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
-import * as React from 'react'
-
-function DropdownMenuGroup({ ...props }: React.ComponentProps<typeof DropdownMenuPrimitive.Group>) {
-  return <DropdownMenuPrimitive.Group data-slot="dropdown-menu-group" {...props} />
-}
-
-export { DropdownMenuGroup }
--- src/shared/components/Atoms/DropdownMenu/DropdownMenuSub.tsx
+++
@@ -1,8 +0,0 @@
-import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
-import * as React from 'react'
-
-function DropdownMenuSub({ ...props }: React.ComponentProps<typeof DropdownMenuPrimitive.Sub>) {
-  return <DropdownMenuPrimitive.Sub data-slot="dropdown-menu-sub" {...props} />
-}
-
-export { DropdownMenuSub }
