8913fc7949c87e5e7a1dccdf98fc93747fd1d013
rubens junio <rubens.marques@hotmart.com>
2025-10-16 13:06:27 -0300

    refactor(welcome): Simplify Wellcome component rendering and heading structure

--- src/features/Wellcome/index.tsx
+++ src/features/Wellcome/index.tsx
@@ -14,15 +14,13 @@ export function Wellcome({ classNames }: { hasDescription?: boolean; classNames?
 
   return (
     <div className={`flex items-start flex-col ${classNames}`}>
-      <h1 className="text-2xl font-bold">
-        {isUserLoading ? (
-          <Skeleton className="w-[250px] h-[30px]" />
-        ) : (
-          <Heading as="h3" level="h3">
-            <Trans i18nKey="init.title" values={{ name }} />
-          </Heading>
-        )}
-      </h1>
+      {isUserLoading ? (
+        <Skeleton className="w-[250px] h-[30px]" />
+      ) : (
+        <Heading as="h3" level="h3" weight="bold">
+          <Trans i18nKey="init.title" values={{ name }} />
+        </Heading>
+      )}
 
       {isUserLoading ? (
         <Skeleton className="w-[150px] h-[20px] mt-2" />
